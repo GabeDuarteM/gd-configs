4313b9d7139635ac1d7ff4e8d1a2ed82a38e8651
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-12-07 17:19:18 +0100

    fix: add babel-transform

---
+++ jest/babel-transform.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+const babelJest = require('babel-jest') // eslint-disable-line import/no-extraneous-dependencies
+
+module.exports = babelJest.createTransformer({
+  presets: [require.resolve('../babel')],
+})
